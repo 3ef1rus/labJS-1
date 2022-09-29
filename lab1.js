ebfd6be84c456f1ad6be9ea7dbb2b2209308eda6
3ef1rus <10kmmrza10k@mail.ru>
2022-09-29 12:34:19 +0300

    complete both exercise

--- lab1.js
+++ lab1.js
@@ -1,6 +1,11 @@
-function checkAge(age){
+function checkAge1(age){
   return (age>18)? true : confirm('Родители разрешили?');
+}
+function checkAge2(age){
+   let result= age>18||confirm('Родители разрешили?');
+   return result;
 }
 
 let age = prompt('Сколько вам лет ?');
-checkAge(age);
+//checkAge1(age);
+checkAge2(age);
